0f1e7fae6d2a05e1f246d7c5c474bb28069b51f4
Kail <darkpilgrim@126.com>
2012-02-26 23:55:54 +0800

    Prepare for asset introduction

--- src/Magend/AssetBundle/Controller/AssetController.php
+++ src/Magend/AssetBundle/Controller/AssetController.php
@@ -23,8 +23,57 @@ class AssetController extends Controller
         return array('name' => $name);
     }
     
-    public function uploadAction()
+    /**
+     * Upload asset
+     * 
+     * @Route("/upload/hot/{id}", name="asset_upload", defaults={"_format" = "json"}, requirements={"id"="\d+"}, options={"expose" = true})
+     */
+    public function uploadAction($id)
     {
+        $repo = $this->getDoctrine()->getRepository('MagendHotBundle:Hot');
+        $hot = $repo->find($id);
+        if (empty($hot)) {
+            return new Response(json_encode(array(
+                'error' => 'hot.not_found'
+            )));
+        }
         
+        $req = $this->getRequest();
+        $file = $req->files->get('file');
+        $ext = $file->guessExtension();
+        if (empty($ext)) {
+            $nameArr = explode('.', $file->getClientOriginalName());
+            $ext = array_pop($nameArr);
+        }
+        
+        $rootDir = $this->container->getParameter('kernel.root_dir');
+        $fileName = uniqid('asset_') . ".$ext";
+        $file->move($rootDir . '/../web/uploads/', $fileName);
+        
+        // @todo @unlink existing files
+        $hotType = $hot->getType();
+        if ($hotType == 0) { // gallery
+            $assets = $hot->getAssets();
+            if (!is_array($assets)) {
+                $assets = array();
+            }
+            $assets[] = array('name' => $file->getClientOriginalName(), 'file' => $fileName);
+        } else {
+            //if ($hotType == 1 || $hotType == 3 || $hotType == 4) { // video audio or single image
+            $assets = $hot->getAssets();
+            if (!empty($assets)) {
+                @unlink($rootDir . '/../web/uploads/' . $assets[0]['file']);
+            }
+            $assets = array(
+                array('name' => $file->getClientOriginalName(), 'file' => $fileName)
+            );
+            //} 
+        }
+
+        $hot->setAssets($assets);
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->flush();
+        
+        return new Response(json_encode($assets));
     }
 }
