ae6c7fd382cd66408c1db24756e1042d90d66d67
Kail <darkpilgrim@126.com>
2012-03-10 19:27:34 +0800

    Create thumbnail for page, and support replace

--- src/Magend/IssueBundle/Util/SimpleImage.php
+++ src/Magend/IssueBundle/Util/SimpleImage.php
@@ -26,7 +26,7 @@ class SimpleImage
     var $image;
     var $image_type;
     
-    function load($filename) {
+    public function load($filename) {
     
       $image_info = getimagesize($filename);
       $this->image_type = $image_info[2];
@@ -42,7 +42,7 @@ class SimpleImage
       }
     }
     
-    function save($filename, $image_type=IMAGETYPE_JPEG, $compression=75, $permissions=null) {
+    public function save($filename, $image_type=IMAGETYPE_JPEG, $compression=75, $permissions=null) {
     
       if( $image_type == IMAGETYPE_JPEG ) {
          imagejpeg($this->image,$filename,$compression);
@@ -59,7 +59,7 @@ class SimpleImage
       }
     }
     
-    function output($image_type=IMAGETYPE_JPEG) {
+    public function output($image_type=IMAGETYPE_JPEG) {
     
       if( $image_type == IMAGETYPE_JPEG ) {
          imagejpeg($this->image);
@@ -72,34 +72,34 @@ class SimpleImage
       }
     }
     
-    function getWidth() {
+    public function getWidth() {
       return imagesx($this->image);
     }
     
-    function getHeight() {
+    public function getHeight() {
       return imagesy($this->image);
     }
     
-    function resizeToHeight($height) {
+    public function resizeToHeight($height) {
     
       $ratio = $height / $this->getHeight();
       $width = $this->getWidth() * $ratio;
       $this->resize($width,$height);
     }
     
-    function resizeToWidth($width) {
+    public function resizeToWidth($width) {
       $ratio = $width / $this->getWidth();
       $height = $this->getheight() * $ratio;
       $this->resize($width,$height);
     }
     
-    function scale($scale) {
+    public function scale($scale) {
       $width = $this->getWidth() * $scale/100;
       $height = $this->getheight() * $scale/100;
       $this->resize($width,$height);
     }
     
-    function resize($width,$height) {
+    public function resize($width,$height) {
       $new_image = imagecreatetruecolor($width, $height);
       imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
       $this->image = $new_image;
