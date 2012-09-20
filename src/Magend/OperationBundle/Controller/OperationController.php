4a90f13078bc0b0c1864a5c4b80c5db0ff742e4a
KailZhang <darkpilgrim@126.com>
2012-09-20 12:16:41 +0800

    Add operation

--- src/Magend/OperationBundle/Controller/OperationController.php
+++ src/Magend/OperationBundle/Controller/OperationController.php
@@ -6,14 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * 
+ * @Route("/operation")
+ * @author Kail
+ */
 class OperationController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/list")
      * @Template()
      */
-    public function indexAction($name)
+    public function listAction()
     {
-        return array('name' => $name);
+        $em = $this->getDoctrine()->getEntityManager();
+        $dql = 'SELECT o, u FROM MagendOperationBundle:Operation o LEFT JOIN o.user u ORDER BY o.createdAt DESC';
+        $q = $em->createQuery($dql)->setMaxResults(10);
+        $operations = $q->getResult();
+        
+        return array('operations' => $operations);
     }
 }
--- src/Magend/OperationBundle/EventListener/OperationEventListener.php
+++ src/Magend/OperationBundle/EventListener/OperationEventListener.php
@@ -6,9 +6,11 @@ use Exception;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\Event;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Magend\MagazineBundle\Entity\Magazine;
 use Magend\IssueBundle\Entity\Issue;
 use Magend\ArticleBundle\Entity\Article;
+use Magend\OperationBundle\Entity\Operation;
 
 /**
  * OperationEventListener
@@ -27,17 +29,58 @@ class OperationEventListener
     public function postPersist(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
-        $entityManager = $args->getEntityManager();
+        $em = $args->getEntityManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $router = $this->container->get('router');
+        $op = null;
         if ($entity instanceof Magazine) {
-            echo 'magazine';exit;
+            $op = new Operation();
+            $op->setUser($user);
+            $url = $router->generate('magazine_issues', array('id' => $entity->getId()));
+            $op->setContent('添加了杂志<a href="' . $url . '">' . $entity->getName() .  '</a>');
+        } else if ($entity instanceof Issue) {
+            $op = new Operation();
+            $op->setUser($user);
+            $url = $router->generate('issue_article_list', array('id' => $entity->getId()));
+            $op->setContent('添加了期刊<a href="' . $url . '">' . $entity->getTitle() .  '</a>');
+        } else if ($entity instanceof Article) {
+            $op = new Operation();
+            $op->setUser($user);
+            $url = $router->generate('article_edit', array('id' => $entity->getId()));
+            $op->setContent('添加了文章<a href="' . $url . '">' . $entity->getTitle() .  '</a>');
+        }
+        if ($op) {
+            $em->persist($op);
+            $em->flush();
         }
     }
     
     public function postUpdate(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
-        $entityManager = $args->getEntityManager();
+        $em = $args->getEntityManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $router = $this->container->get('router');
+        $op = null;
         if ($entity instanceof Magazine) {
+            $op = new Operation();
+            $op->setUser($user);
+            $url = $router->generate('magazine_issues', array('id' => $entity->getId()));
+            $op->setContent('编辑了杂志<a href="' . $url . '">' . $entity->getName() .  '</a>');
+        } else if ($entity instanceof Issue) {
+            $op = new Operation();
+            $op->setUser($user);
+            $url = $router->generate('issue_article_list', array('id' => $entity->getId()));
+            $op->setContent('编辑了期刊<a href="' . $url . '">' . $entity->getTitle() .  '</a>');
+        } else if ($entity instanceof Article) {
+            $op = new Operation();
+            $op->setUser($user);
+            $url = $router->generate('article_edit', array('id' => $entity->getId()));
+            $op->setContent('编辑了文章<a href="' . $url . '">' . $entity->getTitle() .  '</a>');
+        }
+        if ($op) {
+            $em->persist($op);
+            $em->flush();
         }
     }
     
@@ -45,8 +88,24 @@ class OperationEventListener
     {
         $entity = $args->getEntity();
         $em = $args->getEntityManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $router = $this->container->get('router');
+        $op = null;
         if ($entity instanceof Magazine) {
-            
+            $op = new Operation();
+            $op->setUser($user);
+            $op->setContent('删除了杂志<strong>' . $entity->getName() .  '</strong>');
+        } else if ($entity instanceof Issue) {
+            $op = new Operation();
+            $op->setUser($user);
+            $op->setContent('删除了期刊<strong>' . $entity->getTitle() .  '</strong>');
+        } else if ($entity instanceof Article) {
+            $op = new Operation();
+            $op->setUser($user);
+            $op->setContent('删除了文章<stong>' . $entity->getTitle() .  '</strong>');
+        }
+        if ($op) {
+            $em->persist($op);
         }
     }
 }
